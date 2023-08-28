91abe2f3e52d9969472702aec71db87aa7af1700
ramywael <ramybakry9@gmail.com>
2023-08-28 19:20:14 +0300

    Create a list of colors to addNoteForm

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -47,32 +47,19 @@ class _AddNoteFormStateState extends State<AddNoteFormState> {
           const SizedBox(
             height: 26,
           ),
-          BlocBuilder<AddNoteCubit,AddNoteState>(
+          const ColorListView(),
+          const SizedBox(
+            height: 26,
+          ),
+          BlocBuilder<AddNoteCubit, AddNoteState>(
             builder: (BuildContext context, state) {
               return CustomButton(
                 isLoading: state is AddNoteLoading ? true : false,
-                onTap: ()  {
-                  if (formKey.currentState?.validate() ?? false) {
-                    formKey.currentState?.save();
-                    DateTime now = DateTime.now();
-                    var currentTime = DateFormat('yyyy-MM-dd – kk:mm').format(now);
-                    //  Navigator.of(context).pop();
-                    var noteModel= NoteModel(
-                      title: title!,
-                      subtitle: subtitle!,
-                      date: currentTime.toString(),
-                      color: Colors.blue.value,
-                    );
-                    BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-                  } else {
-                    setState(() {
-                      autoValidateMode = AutovalidateMode.always;
-                    });
-                  }
+                onTap: () {
+                  validateMethod(context);
                 },
               );
             },
-
           ),
           const SizedBox(
             height: 26,
@@ -81,7 +68,91 @@ class _AddNoteFormStateState extends State<AddNoteFormState> {
       ),
     );
   }
+
+  void validateMethod(BuildContext context) {
+    if (formKey.currentState?.validate() ?? false) {
+      formKey.currentState?.save();
+      DateTime now = DateTime.now();
+      var currentTime = DateFormat('yyyy-MM-dd – kk:mm').format(now);
+      //  Navigator.of(context).pop();
+      var noteModel = NoteModel(
+        title: title!,
+        subtitle: subtitle!,
+        date: currentTime.toString(),
+        color: Colors.blue.value,
+      );
+      BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+    } else {
+      setState(() {
+        autoValidateMode = AutovalidateMode.always;
+      });
+    }
+  }
 }
 
+class ColorNoteItem extends StatelessWidget {
+  const ColorNoteItem({Key? key, required this.isActive, required this.color}) : super(key: key);
+  final bool isActive;
+  final Color color ;
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ?  CircleAvatar(
+            radius: 35,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: 31,
+            ),
+          )
+        :  CircleAvatar(
+            backgroundColor: color,
+            radius: 34,
+          );
+  }
+}
 
+class ColorListView extends StatefulWidget {
+  const ColorListView({Key? key}) : super(key: key);
 
+  @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int currentIndex = 0;
+  List<Color> colors=const[
+    Color(0xffC2E812),
+    Color(0xff91F5AD),
+    Color(0xff8B9EB7),
+    Color(0xff745296),
+    Color(0xff632A50),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 35 * 2,
+      child: ListView.builder(
+        itemCount: colors.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: GestureDetector(
+              onTap: () {
+                setState(
+                  () {
+                    currentIndex = index;
+                  },
+                );
+              },
+              child: ColorNoteItem(
+                isActive: currentIndex == index, color: colors[index],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
